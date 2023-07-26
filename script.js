e4f63c13836dd629f1268c95938ea0694f98e17e
ZiggyMolteni <molteni@chapman.edu>
2023-07-26 12:37:11 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript code for the interactive animation
+// You can customize this based on your preferences
+
+// Example: Change background color on button click
+const button = document.querySelector('button');
+button.addEventListener('click', function() {
+    document.body.style.backgroundColor = 'pink';
+});
